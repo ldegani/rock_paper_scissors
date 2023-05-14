c21f9b3c844ddcb58e078de34703c4fd2b4fdb2c
Lucas Degani <57118989+ldegani@users.noreply.github.com>
2023-05-14 11:23:03 -0300

    Add all the HTML, CSS and DOM for the GUI

--- script.js
+++ script.js
@@ -1,70 +1,103 @@
-// function to randomly make the computer choice for the game
+const playerSelection = document.querySelectorAll(".btnGame");
+const rounds = document.querySelector(".rounds");
+const btnPlayAgain = document.querySelector(".btnPlayAgain")
+
+// Initialize all counters and variables with a value of 0(zero)
+let playerScore = 0;
+let computerScore = 0;
+let drawScore = 0;
+let roundsCounter = 0;
+
+function playGame () {
+  let playerChoice;
+  playerSelection.forEach((selection) => {
+    selection.addEventListener('click', () => {
+      if (selection === btnOne) {
+        playerChoice = 'odin';
+      } else if (selection === btnTwo) {
+        playerChoice = 'valkyrie'
+      } else if (selection === btnThree) {
+        playerChoice = 'warlord'
+      }
+    roundsCounter++;
+    const computerChoice = getComputerChoice();
+    changeComputerInfo(computerChoice);
+    playRound(computerChoice, playerChoice);
+    if (playerScore === 5 || computerScore === 5) { endGame(); };
+    resetGame();
+    })
+  })
+}
+
+// function to get the computer choice randomly
 function getComputerChoice() {
-  const choices = ["rock", "paper", "scissor"]  // array with all the options to choose
+  const choices = ["odin", "valkyrie", "warlord"]  // array with all the options to choose
   const index = Math.floor(Math.random() * choices.length)  // variable to store the index of the array that was chosen
 
-  return choices[index];  // returns the value using the index to show the string in the position of the array
+  return choices[index];// returns the value using the index to show the string in the position of the array
+}
+
+// function to change the screen infos on the computer choices
+function changeComputerInfo(computerChoice) {
+  var img = document.getElementById("enemy-img");
+  const enemyChoice = document.querySelector(".enemy-choice");  
+
+  if (computerChoice === "odin") {
+    img.src = "./images/odin.png";
+    enemyChoice.innerText = "Odin"
+  } else if (computerChoice === "valkyrie") {
+    img.src = "./images/valkyrie.png";
+    enemyChoice.innerText = "Valkyrie"
+  } else {
+    img.src = "./images/warlord.png";
+    enemyChoice.innerText = "Warlord"
+  }
 }
 
 // function to check who won the round, giving the score and returning a string with the winner
 function playRound(computerChoice, playerChoice) {
-  if (playerChoice === "rock" && computerChoice === "scissors" || playerChoice === "paper" && computerChoice === "rock" || playerChoice === "scissor" && computerChoice === "paper") {
+  rounds.innerText = `Round: ${roundsCounter}`
+
+  if (playerChoice === "odin" && computerChoice === "valkyrie" || playerChoice === "valkyrie" && computerChoice === "warlord" || playerChoice === "warlord" && computerChoice === "odin") {
     playerScore++;
-    return console.log("You won a point!")
   } else if (playerChoice === computerChoice) {
     drawScore++;
-    return console.log("It was a draw!")
   } else {
     computerScore++;
-    return console.log("Computer won a point!")
   }
-}
 
-// function for the game
-function game() {
-  counter++;  // counter to check how many times it was played
-  const playerChoice = prompt("What's you choice? Rock, paper or scissor?").toLowerCase()  // prompt to get the players choice
-  // and store it in a variable all in lower case
-  const computerChoice = getComputerChoice() // stores the computer choice in a variable to be used
-
-  console.log(`Player has chosen ${playerChoice}`)  // message in the console to show the players choice
-  console.log(`Computer has chosen ${computerChoice}`)  // message in the console to know the choices made for the round
-  playRound(computerChoice, playerChoice)  // uses the variables in the function to see who won
-  console.log(`
-  Scoreboard: 
-  Player: ${playerScore} points.
-  Computer: ${computerScore} points.
-  Draws: ${drawScore}.
-  `)  // shows actualized score
-
-  // condition to check if the game has ended
-  if (counter === 5) {
-    endMessage();  // calls the function to give the final score
-  }
+  const wins = document.querySelector('.wins');
+  wins.innerText = `Your Wins: ${playerScore} | Enemy Wins: ${computerScore} | Draws: ${drawScore}`
 }
 
 // function to get the final message with the total score
-function endMessage() {
-  let message = (playerScore > computerScore) ? '\nPlayer won the game!' :
-  (playerScore < computerScore) ? '\nComputer won the game!' :
-    '\nThe game was a draw!';
-  console.log(message);
+function endGame() {
+  var img = document.getElementById("end-img");
+  const message = document.querySelector(".end-message")
 
-  console.log(`
-  Final scoreboard: 
-  Player: ${playerScore} points.
-  Computer: ${computerScore} points.
-  Draws: ${drawScore}.
-  `)  // actualized score
+  // disable the game buttons after all rounds are played
+  playerSelection.forEach((selection) => {
+    selection.setAttribute('disabled', '');
+  })
+
+  btnPlayAgain.removeAttribute('disabled', '');
+  btnPlayAgain.classList.add('on')
+
+  if (playerScore === 5) {
+    img.src = "./images/valhalla.png";
+    img.style.border = "2px solid #FFD700"
+    message.innerText = `You Won! You can go to Valhalla now!`
+  } else if (computerScore === 5) {
+    img.src = "./images/mead.png";
+    img.style.border = "2px solid #bf0603"
+    message.innerText = `You lost! Your enemy is commemorating!`
+  }
 }
 
-// Initialize all counters with a value of 0(zero)
-let playerScore = 0;
-let computerScore = 0;
-let drawScore = 0;
-let counter = 0;
-
-// loop to play the game function 5 times
-for (let i = 0; i < 5; i++) {
-  game()
+function resetGame() {
+  btnPlayAgain.addEventListener('click', () => {
+    window.location.reload();
+  });
 }
+
+playGame();
